9823615c273b19a18e16fc5a73f662cf21654e97
fabiosacerdote <fabygamer0@gmail.com>
2016-07-01 19:12:26 +0200

    Update MainActivity.java
    dovrebbe andare

--- app/src/main/java/app/stefa/rpi_alpha/MainActivity.java
+++ app/src/main/java/app/stefa/rpi_alpha/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        //shish
+        //shish coglionesss
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
