e450adee97097e4a1e507a17895d1ed67201f8fd
fabiosacerdote <fabygamer0@gmail.com>
2016-07-01 19:17:37 +0200

    Update MainActivity.java

--- app/src/main/java/app/stefa/rpi_alpha/MainActivity.java
+++ app/src/main/java/app/stefa/rpi_alpha/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        //shish ciaoo
+        //shish cogli9onjeeee
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
